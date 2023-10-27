f66fe34455ec8261b7595a20fe58a01e9f9afa22
WazzUp05 <vasyaribakov1@gmail.com>
2023-10-27 23:33:32 +0600

    optimization css, add css module

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Counter.scss';
+import classes from './Counter.module.scss';
 export const Counter = () => {
     const [count, setCount] = useState(0);
 
@@ -12,7 +12,9 @@ export const Counter = () => {
     };
     return (
         <div>
-            <button onClick={increment}>increment</button>
+            <button className={classes.btn} onClick={increment}>
+                increment
+            </button>
             {count}
             <button onClick={decrement}>decrement</button>
         </div>
