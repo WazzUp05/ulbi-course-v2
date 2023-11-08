835d7540733171c402e9660a57f2ed1c712e95c6
WazzUp05 <vasyaribakov1@gmail.com>
2023-11-08 21:34:56 +0600

    create Sidebar and mini fixs

---
+++ src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+// import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Sidebar.module.scss';
+import { useState } from 'react';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwither';
+
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
+    //    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+            <button onClick={onToggle}>toggle</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+            </div>
+        </div>
+    );
+};
