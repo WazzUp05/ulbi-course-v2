cb98fc464451156519d8a0d9a5490b09a1064374
Vassiliy Rybakov <vasyaribakov1@gmail.com>
2024-02-16 23:43:32 +0600

    add loki

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,9 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
-    return (
-        <div>
-            <BugButton /> {t('Main page')}
-        </div>
-    );
+    return <div>{t('Main page')}</div>;
 };
 
 export default MainPage;
