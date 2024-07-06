0795b56f07b8a44d55a9696773eb08b9b162cc2b
Vassiliy Rybakov <vasyaribakov1@gmail.com>
2024-07-06 23:30:09 +0500

    try fix again

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -13,7 +13,7 @@ export default {
     moduleDirectories: ['node_modules'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
     rootDir: '../../',
-    modulePaths: ['<rootDir>/src'],
+    modulePaths: ['<rootDir>src'],
     testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
     setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
     moduleNameMapper: {
