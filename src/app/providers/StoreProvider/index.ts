b08556af7b82e724c5584e2c307bc644e795ba4d
Vassiliy Rybakov <vasyaribakov1@gmail.com>
2024-07-13 21:45:22 +0500

    add redux store, add first entity

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,5 @@
+import { createReduxStore } from './config/store';
+import { type StateSchema } from './config/StateSchema';
+import { StoreProvider } from './ui/StoreProvider';
+
+export { StoreProvider, createReduxStore, type StateSchema };
---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,18 @@
+import { Provider } from 'react-redux';
+import { createReduxStore } from '../config/store';
+import { type StateSchema } from '../config/StateSchema';
+import { type ReactNode } from 'react';
+import { type DeepPartial } from '@reduxjs/toolkit';
+
+interface StoreProviderProps {
+    children?: ReactNode;
+    initialState?: DeepPartial<StateSchema>;
+}
+
+export const StoreProvider = (props: StoreProviderProps) => {
+    const { children, initialState } = props;
+
+    const store = createReduxStore(initialState as StateSchema);
+
+    return <Provider store={store}>{children}</Provider>;
+};
