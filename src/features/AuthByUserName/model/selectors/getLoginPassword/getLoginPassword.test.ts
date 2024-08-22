06fb276cf5408ff4ca42be669be3b8bca97739e1
Vassiliy Rybakov <vasyaribakov1@gmail.com>
2024-08-22 15:06:26 +0500

    add tests for login module

---
+++ src/features/AuthByUserName/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,19 @@
+import { type DeepPartial } from '@reduxjs/toolkit';
+import { type StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('should return password', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: '',
+                password: '123123',
+            },
+        };
+        expect(getLoginPassword(state as StateSchema)).toEqual('123123');
+    });
+    test('should return empty string', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
---
+++ src/features/AuthByUserName/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,19 @@
+import { type DeepPartial } from '@reduxjs/toolkit';
+import { type StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUsername } from './getLoginUsername';
+
+describe('getLoginUsername.test', () => {
+    test('should return username', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'name',
+                password: '',
+            },
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('name');
+    });
+    test('should return empty string', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
